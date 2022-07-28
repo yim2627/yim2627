cc1819487671871a2a259f2874da28fa9eea40c6
LIMJISEONG <yim2627@gmail.com>
2022-07-29 03:15:06 +0900

    Update github_action_for_post.py

--- github_action_for_post.py
+++ github_action_for_post.py
@@ -24,7 +24,7 @@ markdown_text = """
 
 [![Hits](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fyim2627%2Fhit-counter&count_bg=%2379C83D&title_bg=%23555555&icon=github.svg&icon_color=%23E7E7E7&title=hits&edge_flat=false)](https://hits.seeyoufarm.com)
 
-### Recent Post..
+### Recent Post...
 
 """
 
